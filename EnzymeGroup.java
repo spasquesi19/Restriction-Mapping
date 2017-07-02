6a89efcebd13ea4e3363f3fb08124a78aefdf266
Sam Pasquesi <spasquesi19@gmail.com>
2017-07-01 23:20:19 -0500

    set up structure of program and implement a check method

---
+++ EnzymeGroup.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class EnzymeGroup here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnzymeGroup
+{
+    Enzyme e1;
+    Enzyme e2;
+    int[] fragLen;
+    /**
+     * Constructor for objects of class EnzymeGroup
+     */
+    public EnzymeGroup(Enzyme e1, Enzyme e2, int[] fragLen)
+    {
+        this.e1=e1;
+        this.e2=e2;
+        this.fragLen=fragLen;
+    }
+    
+    
+}
